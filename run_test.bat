4d3c6785ce0a3fe239048ab0f43a4d0e64094b31
Leon68 <leon-dev@ukr.net>
2023-12-03 15:15:43 +0200

    add git-ignore

---
+++ run_test.bat
@@ -0,0 +1,14 @@
+echo off
+
+echo ################################################################
+echo TestPower_VC.exe
+TestPower_VC.exe
+
+echo.
+echo ################################################################
+echo TestPower_CS.exe
+TestPower_CS.exe
+
+echo ################################################################
+echo TestPowerNetCore.exe
+TestPowerNetCore.exe
