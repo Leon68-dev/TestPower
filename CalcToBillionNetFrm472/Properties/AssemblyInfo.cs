2c32e682f27e109c5ff06a5120618ce505fe4462
Leon68-dev <leon-dev@ukr.net>
2025-03-10 16:52:22 +0200

    changing

--- CalcToBillionNetFrm472/Properties/AssemblyInfo.cs
+++ CalcToBillionNetFrm472/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp2")]
+[assembly: AssemblyTitle("CalcToBillionNetFrm472")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp2")]
+[assembly: AssemblyProduct("CalcToBillionNetFrm472")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
