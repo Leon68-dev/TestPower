a160e8d692f9cf2db07309c2a00394831dbc267d
Leon68-dev <leon-dev@ukr.net>
2025-05-12 19:46:46 +0300

    changing (cmd)

---
+++ TestPowerSumNetCoreMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestPowerSumNetCoreMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TestPowerSumNetCoreMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestPowerSumNetCoreMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
