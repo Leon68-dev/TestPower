ba2f02f2167cb9a308d0f005e48b16836c69835d
Leon68-dev <leon-dev@ukr.net>
2026-02-23 12:20:52 +0200

    changing (cmd)

--- SimpleCpuTestCPP/SimpleCpuTestCPP.cpp
+++ SimpleCpuTestCPP/SimpleCpuTestCPP.cpp
@@ -56,7 +56,6 @@ private:
         return "Benchmark_" + string(timeBuf) + "_" + cpuInfo + ".log";
     }
 
-    // Перейменовано на Log (з великої літери), щоб не було конфлікту з математичним log()
     void Log(const string& message = "", bool newLine = true)
     {
         cout << message;
@@ -182,24 +181,81 @@ public:
         vector<double> r8, r16, r32, overall;
 
         volatile int8_t s1 = 10, s2 = 5, sRes;
-        r8.push_back(runMipsTest("Execute 8 bit Additions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) sRes = s1 + s2; }));
-        r8.push_back(runMipsTest("Execute 8 bit Substractions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) sRes = s1 - s2; }));
-        r8.push_back(runMipsTest("Execute 8 bit Multiplications", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) sRes = s1 * s2; }));
-        r8.push_back(runMipsTest("Execute 8 bit Divisions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) sRes = s1 / s2; }));
+        r8.push_back(runMipsTest("Execute 8 bit Additions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    sRes = s1 + s2;
+            }));
+
+        r8.push_back(runMipsTest("Execute 8 bit Substractions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    sRes = s1 - s2;
+            }));
+
+        r8.push_back(runMipsTest("Execute 8 bit Multiplications", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    sRes = s1 * s2;
+            }));
+
+        r8.push_back(runMipsTest("Execute 8 bit Divisions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    sRes = s1 / s2;
+            }));
         printAverage("8 bit (Short Inteder) arithmetic operations", r8);
 
         volatile int16_t h1 = 10, h2 = 5, hRes;
-        r16.push_back(runMipsTest("Execute 16 bit Additions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) hRes = h1 + h2; }));
-        r16.push_back(runMipsTest("Execute 16 bit Substractions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) hRes = h1 - h2; }));
-        r16.push_back(runMipsTest("Execute 16 bit Multiplications", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) hRes = h1 * h2; }));
-        r16.push_back(runMipsTest("Execute 16 bit Divisions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) hRes = h1 / h2; }));
+        r16.push_back(runMipsTest("Execute 16 bit Additions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    hRes = h1 + h2;
+            }));
+
+        r16.push_back(runMipsTest("Execute 16 bit Substractions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    hRes = h1 - h2;
+            }));
+
+        r16.push_back(runMipsTest("Execute 16 bit Multiplications", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    hRes = h1 * h2;
+            }));
+
+        r16.push_back(runMipsTest("Execute 16 bit Divisions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    hRes = h1 / h2;
+            }));
         printAverage("16 bit (Small Inteder) arithmetic operations", r16);
 
         volatile int32_t i1 = 10, i2 = 5, iRes;
-        r32.push_back(runMipsTest("Execute 32 bit Additions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) iRes = i1 + i2; }));
-        r32.push_back(runMipsTest("Execute 32 bit Substractions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) iRes = i1 - i2; }));
-        r32.push_back(runMipsTest("Execute 32 bit Multiplications", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) iRes = i1 * i2; }));
-        r32.push_back(runMipsTest("Execute 32 bit Divisions", INT_OPERATIONS, [&]() { for (long long i = 0; i < INT_OPERATIONS; i++) iRes = i1 / i2; }));
+        r32.push_back(runMipsTest("Execute 32 bit Additions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    iRes = i1 + i2;
+            }));
+
+        r32.push_back(runMipsTest("Execute 32 bit Substractions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    iRes = i1 - i2;
+            }));
+
+        r32.push_back(runMipsTest("Execute 32 bit Multiplications", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    iRes = i1 * i2;
+            }));
+
+        r32.push_back(runMipsTest("Execute 32 bit Divisions", INT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < INT_OPERATIONS; i++)
+                    iRes = i1 / i2;
+            }));
         printAverage("32 bit (Long Inteder) arithmetic operations", r32);
 
         overall.insert(overall.end(), r8.begin(), r8.end());
@@ -214,25 +270,82 @@ public:
         vector<double> r32, r64, r128, overall;
 
         volatile float f1 = 1.23f, f2 = 4.56f, fRes;
-        r32.push_back(runMipsTest("Execute 32 bit (Single) Additions", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) fRes = f1 + f2; }));
-        r32.push_back(runMipsTest("Execute 32 bit (Single) Substractions", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) fRes = f1 - f2; }));
-        r32.push_back(runMipsTest("Execute 32 bit (Single) Multiplications", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) fRes = f1 * f2; }));
-        r32.push_back(runMipsTest("Execute 32 bit (Single) Divisions", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) fRes = f1 / f2; }));
+        r32.push_back(runMipsTest("Execute 32 bit (Single) Additions", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    fRes = f1 + f2;
+            }));
+
+        r32.push_back(runMipsTest("Execute 32 bit (Single) Substractions", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    fRes = f1 - f2;
+            }));
+
+        r32.push_back(runMipsTest("Execute 32 bit (Single) Multiplications", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    fRes = f1 * f2;
+            }));
+
+        r32.push_back(runMipsTest("Execute 32 bit (Single) Divisions", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    fRes = f1 / f2;
+            }));
         printAverage("32 bit (Single) arithmetic operations", r32);
 
         volatile double d1 = 1.23, d2 = 4.56, dRes;
-        r64.push_back(runMipsTest("Execute 64 bit (Double) Additions", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) dRes = d1 + d2; }));
-        r64.push_back(runMipsTest("Execute 64 bit (Double) Substractions", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) dRes = d1 - d2; }));
-        r64.push_back(runMipsTest("Execute 64 bit (Double) Multiplications", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) dRes = d1 * d2; }));
-        r64.push_back(runMipsTest("Execute 64 bit (Double) Divisions", FLOAT_OPERATIONS, [&]() { for (long long i = 0; i < FLOAT_OPERATIONS; i++) dRes = d1 / d2; }));
+        r64.push_back(runMipsTest("Execute 64 bit (Double) Additions", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    dRes = d1 + d2;
+            }));
+
+        r64.push_back(runMipsTest("Execute 64 bit (Double) Substractions", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    dRes = d1 - d2;
+            }));
+
+        r64.push_back(runMipsTest("Execute 64 bit (Double) Multiplications", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    dRes = d1 * d2;
+            }));
+
+        r64.push_back(runMipsTest("Execute 64 bit (Double) Divisions", FLOAT_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FLOAT_OPERATIONS; i++)
+                    dRes = d1 / d2;
+            }));
         printAverage("64 bit (Double) arithmetic operations", r64);
 
         volatile long double ld1 = 1.23L, ld2 = 4.56L, ldRes;
         long long ldOps = FLOAT_OPERATIONS / 10;
-        r128.push_back(runMipsTest("Execute 128 bit (Extended) Additions", ldOps, [&]() { for (long long i = 0; i < ldOps; i++) ldRes = ld1 + ld2; }));
-        r128.push_back(runMipsTest("Execute 128 bit (Extended) Substractions", ldOps, [&]() { for (long long i = 0; i < ldOps; i++) ldRes = ld1 - ld2; }));
-        r128.push_back(runMipsTest("Execute 128 bit (Extended) Multiplications", ldOps, [&]() { for (long long i = 0; i < ldOps; i++) ldRes = ld1 * ld2; }));
-        r128.push_back(runMipsTest("Execute 128 bit (Extended) Divisions", ldOps, [&]() { for (long long i = 0; i < ldOps; i++) ldRes = ld1 / ld2; }));
+        r128.push_back(runMipsTest("Execute 128 bit (Extended) Additions", ldOps, [&]()
+            {
+                for (long long i = 0; i < ldOps; i++)
+                    ldRes = ld1 + ld2;
+            }));
+
+        r128.push_back(runMipsTest("Execute 128 bit (Extended) Substractions", ldOps, [&]()
+            {
+                for (long long i = 0; i < ldOps; i++)
+                    ldRes = ld1 - ld2;
+            }));
+
+        r128.push_back(runMipsTest("Execute 128 bit (Extended) Multiplications", ldOps, [&]()
+            {
+                for (long long i = 0; i < ldOps; i++)
+                    ldRes = ld1 * ld2;
+            }));
+
+        r128.push_back(runMipsTest("Execute 128 bit (Extended) Divisions", ldOps, [&]()
+            {
+                for (long long i = 0; i < ldOps; i++)
+                    ldRes = ld1 / ld2;
+            }));
         printAverage("128 bit (Extended) arithmetic operations", r128);
 
         overall.insert(overall.end(), r32.begin(), r32.end());
@@ -246,23 +359,59 @@ public:
         Log("* SQR & SQRT ARITHMETIC FUNCTIONS:");
         vector<double> results;
         volatile double num = 1234567890.0, dummyRes;
-        results.push_back(runMipsTest("Test Square of 1234567890", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = num * num; }));
-        results.push_back(runMipsTest("Test Square root of 1234567890", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = sqrt(num); }));
+        results.push_back(runMipsTest("Test Square of 1234567890", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = num * num;
+            }));
+
+        results.push_back(runMipsTest("Test Square root of 1234567890", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = sqrt(num);
+            }));
         printAverage("SQR & SQRT ARITHMETIC FUNCTIONS", results);
 
         Log("* TRIGONOMETRIC FUNCTIONS:");
         results.clear();
-        results.push_back(runMipsTest("Sinus", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = sin(0.785); }));
-        results.push_back(runMipsTest("Cosinus", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = cos(0.785); }));
-        results.push_back(runMipsTest("Tangent", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = tan(0.785); }));
-        results.push_back(runMipsTest("Arcus Tangent", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = atan(0.785); }));
+        results.push_back(runMipsTest("Sinus", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = sin(0.785);
+            }));
+
+        results.push_back(runMipsTest("Cosinus", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = cos(0.785);
+            }));
+
+        results.push_back(runMipsTest("Tangent", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = tan(0.785);
+            }));
+
+        results.push_back(runMipsTest("Arcus Tangent", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = atan(0.785);
+            }));
         printAverage("TRIGONOMETRIC FUNCTIONS", results);
 
         Log("* LOGARITHMIC FUNCTIONS:");
         results.clear();
-        // Тепер тут викликається математичний log(), бо функція класу називається Log
-        results.push_back(runMipsTest("Natural Logarithm", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = log(100.0); }));
-        results.push_back(runMipsTest("Exponent", FUNC_OPERATIONS, [&]() { for (long long i = 0; i < FUNC_OPERATIONS; i++) dummyRes = exp(10.0); }));
+        results.push_back(runMipsTest("Natural Logarithm", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = log(100.0);
+            }));
+
+        results.push_back(runMipsTest("Exponent", FUNC_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < FUNC_OPERATIONS; i++)
+                    dummyRes = exp(10.0);
+            }));
         printAverage("LOGARITHMIC FUNCTIONS", results);
     }
 
@@ -271,10 +420,29 @@ public:
         Log("* LOGICAL INSTRUCTIONS:");
         vector<double> results;
         volatile int32_t v1 = 0xAAAAAAAA, v2 = 0x55555555, vRes;
-        results.push_back(runMipsTest("Logical AND", LOGICAL_OPERATIONS, [&]() { for (long long i = 0; i < LOGICAL_OPERATIONS; i++) vRes = v1 & v2; }));
-        results.push_back(runMipsTest("Logical OR", LOGICAL_OPERATIONS, [&]() { for (long long i = 0; i < LOGICAL_OPERATIONS; i++) vRes = v1 | v2; }));
-        results.push_back(runMipsTest("Logical XOR", LOGICAL_OPERATIONS, [&]() { for (long long i = 0; i < LOGICAL_OPERATIONS; i++) vRes = v1 ^ v2; }));
-        results.push_back(runMipsTest("Logical NOT", LOGICAL_OPERATIONS, [&]() { for (long long i = 0; i < LOGICAL_OPERATIONS; i++) vRes = ~v1; }));
+        results.push_back(runMipsTest("Logical AND", LOGICAL_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < LOGICAL_OPERATIONS; i++)
+                    vRes = v1 & v2;
+            }));
+
+        results.push_back(runMipsTest("Logical OR", LOGICAL_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < LOGICAL_OPERATIONS; i++)
+                    vRes = v1 | v2;
+            }));
+
+        results.push_back(runMipsTest("Logical XOR", LOGICAL_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < LOGICAL_OPERATIONS; i++)
+                    vRes = v1 ^ v2;
+            }));
+
+        results.push_back(runMipsTest("Logical NOT", LOGICAL_OPERATIONS, [&]()
+            {
+                for (long long i = 0; i < LOGICAL_OPERATIONS; i++)
+                    vRes = ~v1;
+            }));
         printAverage("LOGICAL INSTRUCTIONS", results);
     }
 
